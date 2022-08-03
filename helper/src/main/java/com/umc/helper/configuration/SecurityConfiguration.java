6fd633aa9ebb50ead85a95bf421eed9f56b49a28
hojune <hpjune0904@gmail.com>
2022-08-03 20:59:19 +0900

    Refactor deprecated webSecurityConfigureAdapter

--- helper/src/main/java/com/umc/helper/configuration/SecurityConfiguration.java
+++ helper/src/main/java/com/umc/helper/configuration/SecurityConfiguration.java
@@ -2,34 +2,28 @@ package com.umc.helper.configuration;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
 @RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfiguration {
 
-    private final DataSource dataSource;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
 
                 // 다음 URL 은 인증 없이 요청 가능
                 .mvcMatchers("/", "/login", "/signup"
                 ).permitAll();
 
-
-
+        return http.build();
     }
 
 
